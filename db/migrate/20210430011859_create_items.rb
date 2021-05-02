8680bd03bd50e0f92fe5b6d405ca88f4bfb5dfec
AyuumiHara <ayuumi027289@gmail.com>
2021-05-02 12:53:00 +0900

    商品出品機能

---
+++ db/migrate/20210430011859_create_items.rb
@@ -0,0 +1,18 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :item_name
+      t.text :item_text
+      t.integer :price
+      t.references :user, foreign_key: true
+
+      t.integer    :category_id     , null: false
+      t.integer    :condition_id    , null: false
+      t.integer    :shipping_fee_id , null: false
+      t.integer    :area_id         , null: false
+      t.integer    :shipping_days_id, null: false
+
+      t.timestamps
+    end
+  end
+end
