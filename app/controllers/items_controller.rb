c4645dc2556290f360edab164ad7d9d00cb3668e
AyuumiHara <ayuumi027289@gmail.com>
2021-05-06 12:36:46 +0900

    商品詳細表示機能修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  # before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order("created_at DESC")
@@ -40,8 +40,8 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name, :item_text, :category_id, :condition_id, :shipping_fee_id, :area_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
   end
 
-  def contributor_confirmation
-    redirect_to root_path unless current_user == @item.user
-  end
+  # def contributor_confirmation
+    # redirect_to root_path unless current_user == @item.user
+  # end
 
 end
