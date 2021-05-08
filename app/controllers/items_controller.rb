88d4d51f4b267ba87fde4993005ffa330f3a3c3f
AyuumiHara <ayuumi027289@gmail.com>
2021-05-08 20:50:22 +0900

    商品情報編集機能実装修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  # before_action :contributor_confirmation, only: [:edit, :update, :destroy]
   before_action :move_to_index, except: [:index, :show, :update]
+  before_action :set_item, only: [:edit, :show]
 
   def index
     @items = Item.order("created_at DESC")
@@ -21,17 +21,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    item = Item.find(params[:id])
-    # item.update(item_params)
-    if item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to root_path
     else
       render :edit
@@ -51,19 +48,15 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name, :item_text, :category_id, :condition_id, :shipping_fee_id, :area_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
   end
 
-  # def contributor_confirmation
-    # redirect_to root_path unless current_user == @item.user_id
-  # end
-
   def move_to_index
-    # unless user_signed_in?
-    # unless @item_user.id == current_user.id
-      # redirect_to action: :index
-    # redirect_to action: :index unless user_signed_in?
     item = Item.find(params[:id])
     if item.user_id != current_user.id
       redirect_to action: :index
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
