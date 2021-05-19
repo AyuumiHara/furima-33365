6aa57938713a48f543f2544e301ab05aa12d766b
AyuumiHara <ayuumi027289@gmail.com>
2021-05-19 12:57:16 +0900

    バリデーション修正

--- app/models/order_buyer.rb
+++ app/models/order_buyer.rb
@@ -7,8 +7,9 @@ class OrderBuyer
     validates :user_id
     validates :item_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
-    validates :area_id, numericality: { other_than: 0, message: 'Select' }
+    validates :area_id, numericality: { other_than: 1, message: 'Select' }
     validates :city
+    validates :house_number
     validates :phone_number, format: { with: /\A\d{11}\z/, message: 'Input only number' }
     validates :token
   end
